afb150058feb0f8b1002dd970ef526fa45974422
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-10-19 21:54:00 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+
+	// name = "Killa Anderson"
+	// fmt.Println(name)
+
+	// name = "Killa Merge"
+	// fmt.Println(name)
+
+	// var name = "Killa Anderson" // type inference
+	// name := "Killa Anderson" // := var replacement first declaration only
+	// fmt.Println(name)
+
+	// name = "Killa Merge"
+	// fmt.Println(name)
+
+	var (
+		firstName = "Killa"
+		lastName  = "Anderson"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+}
