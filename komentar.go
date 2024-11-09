3831e117514454ddf84b769683a0c6582122d0bb
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-11-09 21:44:53 +0700

    go comment

---
+++ komentar.go
@@ -0,0 +1,14 @@
+// komentar terbaik pada kode adalah kode itu sendiri
+// saat membuat kode kita perlu membuat kode semudah mungkin untuk dibaca
+// namum kadang kita butuh menambahkan komentar di kode kita
+
+/**
+Multiline comment
+ini adalah komentar multiline
+*/
+
+package main
+
+func main() {
+	// this is single line comment
+}
