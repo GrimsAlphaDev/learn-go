5b005c55c027f972b018d621c760a5b58ecc66c3
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-11-10 22:47:09 +0700

    note blank identifier

--- init.go
+++ init.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"learn-basic-golang/database"
-	_ "learn-basic-golang/internal"
+	_ "learn-basic-golang/internal" // menggunakan blank identifier (_) ketika ingin mengimport initnya saja tidak menggunakan method apapun agar tidak dihapus secara otomatis
 )
 
 func main() {
