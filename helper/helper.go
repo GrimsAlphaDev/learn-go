64d29dab9e546b0dea7d3bfc0da41419058cb590
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-11-10 22:40:25 +0700

    access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,18 @@
 package helper // nama package sesuai folder
+import "fmt"
+
+var version = "1.0.0"
+var Application = "golang"
+
+func sayGoodBye(name string) string {
+	return "Good bye " + name
+}
 
 func SayHello(name string) string {
 	return "Hello " + name
+}
+
+func Contoh() {
+	sayGoodBye("test") // dapat diakses dialam package only jika huruf didepan kecil
+	fmt.Println(version)
 }
--- import.go
+++ import.go
@@ -9,4 +9,7 @@ func main() {
 	gerard := helper.SayHello("gerard")
 	fmt.Println(gerard)
 
+	fmt.Println(helper.Application) // huruf depan besar dapat diakses diluar package
+	fmt.Println(helper.version)     // huruf depan kecil tidak bisa diakses diluar package
+
 }
